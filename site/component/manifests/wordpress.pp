52038d4ced732e198899f4154d11ba96d61d3363
AndreasZeissner <zeissnerandreas@googlemail.com>
2015-10-19 22:37:34 +0200

    WP_MOD_SIMPLE - DONE

--- site/component/manifests/wordpress.pp
+++ site/component/manifests/wordpress.pp
@@ -1,5 +1,40 @@
 class component::wordpress (
-
+  $path             = hiera('path', '/var/www/app_name'),
+  $vhost            = hiera('vhost', 'app-name.dev'),
+  $vhost_port       = 80,
+  $ssl              = false,
+  $mutisite         = false,
+  $database         = undef,
+  $php_extension    = {
+                      'mysql' => {ensure  => 'present'},
+                      'curl'  => {ensure  => 'present'},
+                      'json'  => {ensure  => 'present'}
+                      }
 ) {
-  notify {"I am WP_MOD_SIMPLE!":}
-}
+  ## create vhost for Wordpress
+  nginx::resource::vhost { "${vhost}-${vhost_port}-wordpress" :
+    ensure      => present,
+    server_name => [$vhost],
+    www_root    => $path,
+    listen_port => $vhost_port,
+  }
+  ## create location to direct .php to the fpm pool
+  nginx::resource::location { 'wasted-php-rewrite':
+    location  => '~ \.php$',
+    vhost     => "${vhost}-${vhost_port}-wordpress",
+    fastcgi   => '127.0.0.1:9000',
+    try_files => ['$uri =404'],
+    www_root  => $path,
+  }
+
+  contain '::mysql::server'
+  contain '::mysql::client'
+  contain '::php'
+
+  # TODO This throws an error but works "Warning: Scope(Php::Extension[json]): php::extension is private"
+
+  create_resources('::php::extension', $php_extension)
+  create_resources('::mysql::db', $database)
+  }
+
+
